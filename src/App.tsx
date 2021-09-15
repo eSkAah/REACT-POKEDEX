96d14ea4949a4e823d9efcff62c4dbb852197482
Allan <allan.breuil@hotmail.fr>
2021-09-15 23:05:22 +0200

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {FunctionComponent, useState, useEffect} from 'react';
 import POKEMONS from './models/mock-pokemon';
 import Pokemon from './models/pokemon';
@@ -9,25 +8,6 @@ const App: FunctionComponent = () => {
 
  return (
 	<PokemonList />
-=======
-import React, {FunctionComponent, useEffect, useState} from 'react';
-import Pokemon from './models/pokemon';
-import POKEMONS from './models/mock-pokemon';
-  
-const App: FunctionComponent = () => {
- const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-
- useEffect(() => {
-  setPokemons(POKEMONS);
- }, []);
-    
- return (
-   <div>
-     <h1>Bienvenu sur votre Pokédex</h1>
-     <p>Vous avez actuellement {pokemons.length} dans votre Pokédex</p>
-   </div>
- 
->>>>>>> 4d6c2dea2c63ff14787ae12553da5751114f9f90
  )
 }
   
