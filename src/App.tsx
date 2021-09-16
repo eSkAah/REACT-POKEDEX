86d503ec129e83e8a4bf446f541c2e839af414eb
Allan <allan.breuil@hotmail.fr>
2021-09-16 11:06:29 +0200

    Routage & PageNotFound & PageDetails Pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import React, {FunctionComponent, useState, useEffect} from 'react';
-import POKEMONS from './models/mock-pokemon';
-import Pokemon from './models/pokemon';
-import PokemonCard from './components/pokemon-card';
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
+import PokemonsDetail from './pages/pokemon-details';
 import PokemonList from './pages/pokemon-list';
+import PageNotFound from './pages/page-not-found';
+
   
 const App: FunctionComponent = () => {
 
  return (
-	<PokemonList />
+	 <Router> {/*Mise en place du système de Routage*/}
+		<div>
+			{/*La barre de navigation commune à toutes les pages*/}
+			<nav>
+				<div className="nav-wrapper teal">
+				   <Link to="/" className="brand-logo center">Pokédex</Link>
+				</div>
+			</nav>
+			{/*Système de géstion des routes de notre application*/}
+			<Switch>
+				<Route exact path="/" component={PokemonList} />
+				<Route exact path="/pokemons" component={PokemonList} />
+				<Route exact path="/pokemons/:id" component={PokemonsDetail} />
+				<Route component={PageNotFound} />
+			</Switch>
+		 </div>
+	 </Router>
+		  
  )
 }
   
